0e3a54f0c3645a16cf03048bb5a9dc0dfb3c11f1
Moody <mooodyhunter@outlook.com>
2021-12-25 17:27:52 +0000

    fix CI

--- APIServer/generate.go
+++ APIServer/generate.go
@@ -1,5 +1,5 @@
 //go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
 //go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
-//go:generate protoc --go_out=api --go_opt=paths=source_relative --go-grpc_out=api --go-grpc_opt=paths=source_relative -I=.. CameraAPI.proto
+//go:generate protoc --go_out=camapi --go_opt=paths=source_relative --go-grpc_out=camapi --go-grpc_opt=paths=source_relative -I=.. CameraAPI.proto
 
 package main
