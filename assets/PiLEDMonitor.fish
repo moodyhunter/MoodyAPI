ef753ccc9f2e69c5e05bbf60dc9b15a8f9d5c702
Moody <mooodyhunter@outlook.com>
2021-12-27 02:07:40 +0000

    fix multiple LEDs

--- assets/PiLEDMonitor.fish
+++ assets/PiLEDMonitor.fish
@@ -2,9 +2,9 @@
 
 while true
     if [ (systemctl show motion | grep 'SubState=') = "SubState=running" ]
-        echo "1" > /sys/class/leds/led?/brightness
+        echo "1" | tee /sys/class/leds/led?/brightness
     end
     sleep 2
-    echo "0" > /sys/class/leds/led?/brightness
+    echo "0" | tee /sys/class/leds/led?/brightness
     sleep 2
 end
