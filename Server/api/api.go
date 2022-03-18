3be9c92e6648fe6e4e86cce78be64dcc23d83fcb
Moody <mooodyhunter@outlook.com>
2022-03-18 20:41:27 +0000

    fix UpdateCameraState API

--- Server/api/api.go
+++ Server/api/api.go
@@ -108,7 +108,7 @@ done:
 	return nil
 }
 
-func (s *APIServer) SetCameraState(ctx context.Context, request *UpdateCameraStateRequest) (*emptypb.Empty, error) {
+func (s *APIServer) UpdateCameraState(ctx context.Context, request *UpdateCameraStateRequest) (*emptypb.Empty, error) {
 	if request == nil || request.Auth == nil || request.Auth.ClientId != common.APISecret {
 		log.Printf("WARNING: Invalid secret from client: %s", request.Auth.ClientId)
 		return nil, errors.New("error: Invalid Secret")
