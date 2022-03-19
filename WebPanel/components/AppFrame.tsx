1273fc596bbf6b3e1edb97c9c66e7771d48523f6
Moody <mooodyhunter@outlook.com>
2022-03-19 16:18:35 +0000

    add simple WebPanel

---
+++ WebPanel/components/AppFrame.tsx
@@ -0,0 +1,67 @@
+import { AppBar as MuiAppBar, AppBarProps as MuiAppBarProps, Drawer as MuiDrawer } from '@mui/material';
+import { CSSObject, styled, Theme } from '@mui/material/styles';
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+export const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
+
+export const AppTopBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        // width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+export const AppDrawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && { ...openedMixin(theme), '& .MuiDrawer-paper': openedMixin(theme) }),
+        ...(!open && { ...closedMixin(theme), '& .MuiDrawer-paper': closedMixin(theme) }),
+    }),
+);
---
+++ WebPanel/pages/_app.tsx
@@ -0,0 +1,75 @@
+import {
+    Home as HomeIcon,
+    Laptop as LaptopIcon,
+    Menu as MenuIcon,
+    Settings as SettingsIcon,
+    NotificationsNone as NotificationIcon,
+    NetworkCheck as NetworkIcon
+} from '@mui/icons-material';
+import { Box, Container, CssBaseline, Divider, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { AppProps } from 'next/app';
+import { ReactElement, useEffect, useState } from 'react';
+import { AppDrawer, AppTopBar, DrawerHeader } from '../components';
+
+type AppListButtonProps = {
+    open: boolean,
+    name: string,
+    icon: ReactElement
+};
+
+function AppListButton(props: AppListButtonProps) {
+    return (
+        <ListItemButton
+            key={props.name} sx={{ minHeight: 48, justifyContent: props.open ? 'initial' : 'center', px: 2.5 }}>
+            <ListItemIcon sx={{ minWidth: 0, mr: props.open ? 3 : 'auto', justifyContent: 'center' }}>
+                {props.icon}
+            </ListItemIcon>
+            <ListItemText primary={props.name} sx={{ opacity: props.open ? 1 : 0 }} />
+        </ListItemButton>
+    );
+}
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => { setOpen(JSON.parse(window.localStorage.getItem('open') ?? "false")); }, []);
+    useEffect(() => { window.localStorage.setItem('open', String(open)); }, [open]);
+
+    const toggleDrawer = () => { setOpen(!open); };
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppTopBar position="fixed" open={open} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        onClick={toggleDrawer}
+                        edge="start"
+                        sx={{ marginRight: 5 }}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        MoodyAPI Dashboard
+                    </Typography>
+                </Toolbar>
+            </AppTopBar>
+            <AppDrawer variant="permanent" open={open}>
+                <DrawerHeader />
+                <List>
+                    <AppListButton name='Home' open={open} icon={(<HomeIcon />)} />
+                    <AppListButton name='Clients' open={open} icon={(<LaptopIcon />)} />
+                    <AppListButton name='Wireguard Clients' open={open} icon={(<NetworkIcon />)} />
+                    <AppListButton name='Notifications' open={open} icon={(<NotificationIcon />)} />
+                </List>
+                <Divider />
+                <List>
+                    <AppListButton name='Settings' open={open} icon={(<SettingsIcon />)} />
+                </List>
+            </AppDrawer>
+
+            <Container>
+                <Component {...pageProps}></Component>
+            </Container>
+        </Box >
+    );
+}
