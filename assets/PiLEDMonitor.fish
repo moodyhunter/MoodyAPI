32730e77d567310ecf0695ddfc5c57a9531a3c8a
Moody <mooodyhunter@outlook.com>
2022-04-04 13:47:25 +0100

    update assets

--- assets/PiLEDMonitor.fish
+++ assets/PiLEDMonitor.fish
@@ -1,22 +1,22 @@
 #!/bin/fish
 
-set oldState "off"
+set oldState off
 ./PiScreen 75 2 "Camera:             Off"
 
 while true
     if [ (systemctl show motion | grep 'SubState=') = "SubState=running" ]
-        echo "1" | tee /sys/class/leds/led1/brightness
-        if [ $oldState = "off" ]
+        echo 1 | tee /sys/class/leds/led1/brightness
+        if [ $oldState = off ]
             ./PiScreen 75 2 "Camera:             On"
-            set oldState "on"
+            set oldState on
         end
     else
-        if [ $oldState = "on" ]
+        if [ $oldState = on ]
             ./PiScreen 75 2 "Camera:             Off"
-            set oldState "off"
-    end
+            set oldState off
+        end
     end
     sleep 2
-    echo "0" | tee /sys/class/leds/led1/brightness
+    echo 0 | tee /sys/class/leds/led1/brightness
     sleep 2
 end
