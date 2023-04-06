2c551e66014f023eeb4a6c1930c100adf6a596cd
Moody Liu <mooodyhunter@outlook.com>
2023-04-06 20:25:31 +0100

    好

--- LightControl/src/main.rs
+++ LightControl/src/main.rs
@@ -1,76 +1,24 @@
 mod fastcon;
+mod models;
 
-use std::{collections::BTreeMap, time::Duration};
+use crate::models::{common::Auth, light::SubscribeLightRequest};
 
-use tokio::time::sleep;
-
-use bluer::{adv::Advertisement, Adapter, AdapterEvent, Address, DeviceEvent, DeviceProperty};
-use fastcon::{
-    broadcast_parser::{parse_ble_broadcast, BroadcastType},
-    light::{BLELight, LightState},
+use bluer::{adv::Advertisement, Adapter};
+use core::panic;
+use fastcon::light::{BLELight, LightState as FastConLightState};
+use ini::Ini;
+use models::{
+    light::{light_state::Mode, LightState},
+    moody_api::moody_api_service_client::MoodyApiServiceClient,
 };
-use futures::{stream::SelectAll, StreamExt};
+use platform_dirs::AppDirs;
+use std::{collections::BTreeMap, error::Error, time::Duration};
+use tokio::time::sleep;
+use tonic::{transport::Channel, Request};
 
-use crate::fastcon::DEFAULT_PHONE_KEY;
-
-const MY_ADDRESS: Address = Address::new([0x11, 0x22, 0x33, 0x44, 0x55, 0x66]);
 const MY_MANUFACTURER_DATA_KEY: u16 = 0xfff0;
 
-fn print_bytes(name: &str, bytes: &[u8]) {
-    print!("{}: ", name);
-    for b in bytes {
-        print!("{:02x} ", b);
-    }
-    println!();
-}
-
-async fn handle_light_event(
-    _adapter: &Adapter,
-    ev: DeviceEvent,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let DeviceEvent::PropertyChanged(prop) = ev;
-
-    match prop {
-        DeviceProperty::Rssi(_) => Ok(()), // ignore RSSI, we don't care
-        DeviceProperty::ManufacturerData(dmap) => {
-            let Some(data) = dmap.get(&MY_MANUFACTURER_DATA_KEY) else {
-                println!("Invalid light bulb manufacturer data");
-                return Ok(());
-            };
-
-            // dump the data
-            println!("    Manufacturer data: {:?}", data);
-
-            // parse the data
-            let Some(x) = parse_ble_broadcast(data, &DEFAULT_PHONE_KEY) else {
-                println!("Invalid light bulb manufacturer data");
-                return Ok(());
-            };
-
-            match x {
-                BroadcastType::HeartBeat(heartbeat) => {
-                    println!("Heartbeat: {:?}", heartbeat);
-                    // single_on_off_command(heartbeat.short_addr, true);
-                }
-                BroadcastType::TimerUploadResponse => {
-                    println!("Timer upload response");
-                }
-                BroadcastType::DeviceAnnouncement(device) => {
-                    println!("Device announcement: {:?}", device);
-                }
-            }
-
-            Ok(())
-        }
-        other => {
-            println!("    Unhandled property: {:?}", other);
-            Ok(())
-        }
-    }
-}
-
 async fn do_advertise(adapter: &Adapter, data: &Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
-    print_bytes("Advertisement Data", data);
     let mut my_data: BTreeMap<u16, Vec<u8>> = BTreeMap::new();
     my_data.insert(MY_MANUFACTURER_DATA_KEY, data.clone());
 
@@ -84,73 +32,107 @@ async fn do_advertise(adapter: &Adapter, data: &Vec<u8>) -> Result<(), Box<dyn s
         tx_power: Some(20),
         ..Default::default()
     };
-    println!("{:?}", &le_advertisement);
-    let handle = adapter.advertise(le_advertisement).await?;
 
-    println!("Removing advertisement");
+    let handle = adapter.advertise(le_advertisement).await?;
     drop(handle);
     sleep(Duration::from_millis(500)).await;
 
     Ok(())
 }
 
-async fn do_poll_device_events(adapter: &Adapter) -> Result<(), Box<dyn std::error::Error>> {
-    let mut device_events = adapter.discover_devices().await?;
-    let mut light_change_events = SelectAll::new();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let dirs = AppDirs::new(Some("moodyapi"), false).unwrap();
+    let ini_path = dirs.config_dir.as_path().join("LightDaemon.ini");
 
-    loop {
-        tokio::select! {
-            Some(de) = device_events.next() => {
-                match de {
-                    AdapterEvent::DeviceAdded(addr) => {
-                        if addr != MY_ADDRESS {
-                            continue;
-                        }
+    let conf = if let Ok(ini_file) = Ini::load_from_file(ini_path.as_path()) {
+        ini_file
+    } else if let Ok(ini_file) = Ini::load_from_file("/etc/moodyapi/LightDaemon.ini") {
+        ini_file
+    } else {
+        panic!("Failed to locate configurations.");
+    };
 
-                        println!("Device added: {addr}");
-                        let device = adapter.device(MY_ADDRESS).expect("Error getting device");
-                        let current_data = device.manufacturer_data().await?;
-                        if let Some(data) = current_data {
-                            handle_light_event(&adapter, DeviceEvent::PropertyChanged(DeviceProperty::ManufacturerData(data))).await.expect("Error handling light event");
-                        }
+    let api_host = conf.general_section().get("Server").unwrap().to_string();
+    let client_id = conf.general_section().get("ClientID").unwrap().to_string();
 
-                        light_change_events.push(device.events().await?);
-                    }
-                    AdapterEvent::DeviceRemoved(addr) => {
-                        if addr != MY_ADDRESS {
-                            continue;
-                        }
-                        device_events = adapter.discover_devices().await?;
-                        println!("Light {addr} fell off the bus");
-                    }
-                    _ => (),
-                }
-            },
-            Some(le) = light_change_events.next() => handle_light_event(&adapter, le).await.expect("Error handling light event"),
-            else => {
-                println!("No more events");
-                std::process::exit(1);
-            }
-        }
-    }
-}
+    let grpc_channel = Channel::from_shared(api_host.clone())?
+        .connect()
+        .await
+        .expect("Can't create a channel");
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let session = bluer::Session::new().await?;
     let adapter = session.default_adapter().await?;
     adapter.set_powered(true).await?;
 
-    let phone_key = vec![0x38, 0x35, 0x36, 0x30];
+    loop {
+        let mut client = MoodyApiServiceClient::new(grpc_channel.clone());
 
-    let mut light = BLELight::new(1, &phone_key);
-    light.set_state(LightState::WarmWhite);
-    light.set_brightness(127);
+        let request = Request::new(SubscribeLightRequest {
+            auth: Some(Auth {
+                client_uuid: client_id.clone(),
+            }),
+            ..Default::default()
+        });
+
+        match client.subscribe_light_state_change(request).await {
+            Ok(stream) => {
+                let mut resp_stream = stream.into_inner();
+                loop {
+                    match resp_stream.message().await {
+                        Ok(Some(l)) => {
+                            println!("Received LightState: {:?}", l);
+                            match send_light_command(&adapter, l).await {
+                                Ok(_) => {}
+                                Err(e) => println!("Failed to send light command: {}", e),
+                            }
+                        }
+                        e => {
+                            println!("something went wrong: {:?}", e);
+                            sleep(Duration::from_secs(1)).await;
+                            break;
+                        }
+                    }
+                }
+            }
+            e => {
+                println!("something went wrong: {:?}", e);
+                sleep(Duration::from_secs(1)).await;
+            }
+        }
+
+        sleep(Duration::from_secs(10)).await;
+    }
+}
+
+const PHONE_KEY: [u8; 4] = [0x38, 0x35, 0x36, 0x30];
+
+async fn send_light_command(
+    adapter: &Adapter,
+    lightstate: LightState,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut light = BLELight::new(1, &PHONE_KEY);
+
+    if lightstate.on {
+        light.set_brightness(lightstate.brightness as u8);
+        match lightstate.mode {
+            Some(Mode::Warmwhite(_)) => {
+                light.set_state(FastConLightState::WarmWhite);
+            }
+            Some(Mode::Colored(color)) => {
+                light.set_state(FastConLightState::RGB(
+                    color.red as u8,
+                    color.green as u8,
+                    color.blue as u8,
+                ));
+            }
+            None => todo!(),
+        }
+    } else {
+        light.set_state(FastConLightState::Off);
+    }
+
     let data = light.get_advertisement();
-
     do_advertise(&adapter, &data).await?;
-
-    do_poll_device_events(&adapter).await?;
-
     Ok(())
 }
