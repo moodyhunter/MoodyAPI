5ac868e95968b1feb644a5c4a85703d9cb7f0f62
Moody Liu <mooodyhunter@outlook.com>
2023-05-07 12:04:33 +0100

    remove android client

--- Client/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-cd build
-
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release
-cmake --build . --parallel
--- Client/cleanup-assets.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-
-for f in assets/*.svg
-    inkscape --export-filename=$f.out.svg -l $f
-    mv -v $f.out.svg $f
-end
-
